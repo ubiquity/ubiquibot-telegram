0d55b578441c590867aadb91b52e2a8a109c4fbc
Seprintour <godstimeoobasi@gmail.com>
2023-08-12 07:02:58 +0100

    fix: missing pat error

--- deploy-keys.js
+++ deploy-keys.js
@@ -1,12 +1,15 @@
 const { spawn } = require("child_process");
 const fs = require("fs");
 
-async function readEnvironmentFile() {
-  try {
+async function readEnvironmentFile()
+{
+  try
+  {
     const environmentData = JSON.parse(fs.readFileSync("environment.json", "utf8"));
     const keys = Object.keys(environmentData);
 
-    for (const key of keys) {
+    for (const key of keys)
+    {
       const value = environmentData[key];
       const command = `echo '${value}' | wrangler secret put ${key.toUpperCase()}`;
 
@@ -24,20 +27,26 @@ async function readEnvironmentFile() {
       // Continue the loop after each command is finished
       console.log(`Command for "${key}" completed.`);
     }
-  } catch (err) {
+  } catch (err)
+  {
     console.error("Error reading or executing commands:", err);
   }
 }
 
-function waitForEvent(emitter, event) {
-  return new Promise((resolve) => {
-    emitter.on(event, (data) => {
+function waitForEvent(emitter, event)
+{
+  return new Promise((resolve) =>
+  {
+    emitter.on(event, (data) =>
+    {
       console.log(data.toString());
     });
-    emitter.once("error", (error) => {
+    emitter.once("error", (error) =>
+    {
       console.error(error.toString());
     });
-    emitter.once("close", (code) => {
+    emitter.once("close", (code) =>
+    {
       resolve(code);
     });
   });
