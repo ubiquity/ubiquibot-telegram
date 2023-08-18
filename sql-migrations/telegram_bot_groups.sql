51f920a7c78779a7aa6dd24afc0e709da5ca8a5f
Seprintour <godstimeoobasi@gmail.com>
2023-08-18 12:52:57 +0100

    chore: bring in sql migrations

---
+++ sql-migrations/telegram_bot_groups.sql
@@ -0,0 +1,8 @@
+CREATE TABLE telegram_bot_groups (
+    id bigint PRIMARY KEY,
+    created_at timestamp with time zone,
+    updated_at timestamp with time zone,
+    group_name text,
+    from_id bigint,
+    github_repo text
+)
