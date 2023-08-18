63f1b5f08a9adfe790f468c51f28c0b09455da5a
Seprintour <godstimeoobasi@gmail.com>
2023-08-18 14:24:30 +0100

    fix: remove trailing comma

--- sql-migrations/telegram_users_table.sql
+++ sql-migrations/telegram_users_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE telegram_users (
   user_id VARCHAR(255) PRIMARY KEY,
   github_id TEXT,
   created_at timestamp with time zone,
-  updated_at timestamp with time zone,
+  updated_at timestamp with time zone
 );
